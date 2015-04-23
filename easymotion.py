160f62b2527caa49269ecd68c05c6036767f2642
Grant Wu <grantwu2@gmail.com>
2015-04-23 00:25:50 -0700

    Search in visible text for word
    Uses re.finditer to search in the visible text for a target. Returns the
    start/end indices for all matches, which can be used to define those matches as
    Regions, which will hopefully let us highlight, choose, and jump to them?

--- easymotion.py
+++ easymotion.py
@@ -1,9 +1,34 @@
+import re
+
 import sublime, sublime_plugin
 
 class LoadTextCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        print(self.view.sel())
+        # Load viewport text as string
+        visible_region = self.view.visible_region()
+        visible_region_string = self.view.substr(visible_region)
 
+        global start
+        start = visible_region.begin()
+
+        start_index = 50
+        end_index = 55
+
+        search = visible_region_string[start_index:end_index]
+        print(search)
+
+        self.find_in_string('core', visible_region_string)
+
+        target_region = sublime.Region(start + start_index, start + end_index)
+        # print(self.view.substr(target_region))
+        # print(visible_region_string[start_index:end_index])
+
+
+    def find_in_string(self, search, target):
+        for m in re.finditer(search, target):
+            target_region = sublime.Region(start + m.start(), start + m.end())
+
+            print(self.view.substr(target_region))
 
 
 
